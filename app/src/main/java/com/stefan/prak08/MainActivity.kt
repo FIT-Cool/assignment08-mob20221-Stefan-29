4f5eb7f00b9937815737ffb1d233342542847dfb
Stefan-29 <58769705+Stefan-29@users.noreply.github.com>
2022-11-24 17:34:23 +0700

    Scrolling Activity can be sent data

--- app/src/main/java/com/stefan/prak08/MainActivity.kt
+++ app/src/main/java/com/stefan/prak08/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.stefan.prak08
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -14,6 +15,7 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 /**
  * @author Stefanus - 2072013, Vina Anjelina - 2072022
  * **
@@ -21,8 +23,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-    private lateinit var articles : ArrayList<Article>
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var articles: ArrayList<Article>
     private lateinit var newsArticleAdapter: NewsArticleAdapter
     val retrofit = Retrofit.Builder()
         .baseUrl("https://newsapi.org/v2/")
@@ -38,9 +40,9 @@ class MainActivity : AppCompatActivity() {
         newsArticleAdapter = NewsArticleAdapter(articles)
 
         //click to view more data
-        newsArticleAdapter.setArticleDataListener(object: NewsArticleAdapter.ArticleDataListener{
+        newsArticleAdapter.setArticleDataListener(object : NewsArticleAdapter.ArticleDataListener {
             override fun articleItemClicked(article: Article) {
-                ShowMoreArticleData(article)
+                showMoreArticleData(article)
             }
         })
 
@@ -52,21 +54,24 @@ class MainActivity : AppCompatActivity() {
             binding.srLayout.isRefreshing = false
         }
     }
+
     override fun onStart() {
         super.onStart()
         fetchArticleData()
     }
 
-    private fun fetchArticleData(){
+    private fun fetchArticleData() {
         val service = retrofit.create(NewsApi::class.java)
-        val call = service.getCurrentNewsData("Twitter", "f0c081794a864a8ca72ebbebc350efc9") //Getting Twitter Related News
+        val call = service.getCurrentNewsData("Twitter",
+            "f0c081794a864a8ca72ebbebc350efc9") //Getting Twitter Related News
         call?.enqueue(object : Callback<ArticlesJson?> {
             override fun onResponse(call: Call<ArticlesJson?>, response: Response<ArticlesJson?>) {
                 response.body()!!.articles?.let {
-                        articles.addAll(it)
+                    articles.addAll(it)
                 }
                 newsArticleAdapter.notifyItemChanged(0)
             }
+
             override fun onFailure(call: Call<ArticlesJson?>, t: Throwable) {
                 Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
 
@@ -76,33 +81,33 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-   private fun ShowMoreArticleData(article: Article){
-       val service = retrofit.create(NewsApi::class.java)
-       val call = service.getCurrentNewsData("Twitter", "f0c081794a864a8ca72ebbebc350efc9") //Getting Twitter Related News
-       call?.enqueue(object : Callback<ArticlesJson?> {
-           override fun onResponse(call: Call<ArticlesJson?>, response: Response<ArticlesJson?>) {
-               response.body()!!.articles?.let {
-                   articles.addAll(it)
-               }
+    private fun showMoreArticleData(article: Article) {
+        val service = retrofit.create(NewsApi::class.java)
+        val call = service.getCurrentNewsData("Twitter",
+            "f0c081794a864a8ca72ebbebc350efc9") //Getting Twitter Related News
+        call?.enqueue(object : Callback<ArticlesJson?> {
+            override fun onResponse(call: Call<ArticlesJson?>, response: Response<ArticlesJson?>) {
+                response.body()!!.articles?.let {
+                    articles.addAll(it)
+                }
 
-               Toast.makeText(this@MainActivity, article.content,
-                   Toast.LENGTH_SHORT).show()
-
-//               response.body()!!.articles?.let {
-//                   if (article.source.id == ){
-//                       val intent = Intent (this@MainActivity,ScrollingActivity::class.java)
-//                       intent.putExtra("article", article)
-//                       startActivity(intent)
-           }
+//               Toast.makeText(this@MainActivity, article.content,
+//                   Toast.LENGTH_SHORT).show()
 
 
-           override fun onFailure(call: Call<ArticlesJson?>, t: Throwable) {
-               Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@MainActivity, ScrollingActivity::class.java)
+                intent.putExtra("news",article)
+                startActivity(intent)
+            }
 
-           }
 
-       })
-  }
+            override fun onFailure(call: Call<ArticlesJson?>, t: Throwable) {
+                Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
+
+            }
+
+        })
+    }
 }
 
 
