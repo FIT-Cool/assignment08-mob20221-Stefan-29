82bf944f0b79730dc12e089f4bf9b9f422c431f1
vinaanjelina <71738548+vinaanjelina@users.noreply.github.com>
2022-11-27 15:18:25 +0700

    Add files via upload
    Testing Code Adapter

---
+++ assignment08-mob20221-Stefan-29-main/app/src/main/java/com/stefan/prak08/Api/NewsApi.kt
@@ -0,0 +1,20 @@
+package com.stefan.prak08.Api
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * @author Stefanus - 2072013
+ * **
+ */
+
+//https://newsapi.org/v2/everything?q=Apple&from=2022-11-20&sortBy=popularity&apiKey=f0c081794a864a8ca72ebbebc350efc9
+
+interface NewsApi {
+    @GET("everything") //get annotation
+    fun getCurrentNewsData(@Query("q") mention: String?, @Query("apiKey") apiKey: String): Call<ArticlesJson?>?
+
+    @GET("v2/everything?q=Apple&from=2022-11-20&sortBy=popularity&apiKey=f0c081794a864a8ca72ebbebc350efc9")
+    fun getNews(@Query("q") mention: String?, @Query("apiKey") apiKey: String): Call<ArticlesJson?>?
+}
---
+++ assignment08-mob20221-Stefan-29-main/app/src/main/java/com/stefan/prak08/Api/Source.kt
@@ -0,0 +1,11 @@
+package com.stefan.prak08.Api
+
+/**
+ * @author Vina Anjelina - 2072022
+ * **
+ */
+
+data class Source(
+    val id: String,
+    val name: String
+)
